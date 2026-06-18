efe08057cf9830d4094028c748ebab4534076d73
Aftabclear <beingaftabaalam@gmail.com>
2026-06-18 11:06:46 +0530

    Best time to buy Stock

---
+++ TimeToBuyStock.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class TimeToBuyStock {
+    public static int Stock(int[] prices){
+
+    }
+    
+    public static void main(String[] args) {
+    int[] prices={7,1,5,3,6,4};
+    System.out.println(Stock(prices));
+    }
+}
