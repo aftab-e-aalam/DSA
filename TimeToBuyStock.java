a8885ecdf88ddb61be573e85aaed541899c221c2
AFTAB AALAM <beingaftabaalam@gmail.com>
2026-06-18 11:10:35 +0530

    Best time to buy stock
    Refactor Stock method to calculate maximum profit from stock prices.

--- TimeToBuyStock.java
+++ TimeToBuyStock.java
@@ -1,6 +1,19 @@
 import java.util.*;
 public class TimeToBuyStock {
     public static int Stock(int[] prices){
+        int buyPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]>buyPrice){
+                int profit=prices[i]-buyPrice;
+                maxProfit=Math.max(profit,maxProfit);
+                
+            }else{
+                buyPrice=prices[i];
+            }
+        }
+        return maxProfit;
 
     }
     
